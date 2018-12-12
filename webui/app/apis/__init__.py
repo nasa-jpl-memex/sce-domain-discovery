ef23438f46a53e7fdc739d67b3559d24262fd1b1
Sujen Shah <sujen1412@users.noreply.github.com>
2018-12-12 10:38:57 -0800

    put swagger docs at /doc

--- webui/app/apis/__init__.py
+++ webui/app/apis/__init__.py
@@ -3,7 +3,7 @@ from flask_restplus import Api
 from app.apis.ns_search import api as search_api
 from app.apis.ns_classify import api as classify_api
 
-api = Api(title='Seed Generation', version='1.0', description='Tool to generate seeds for Domain Discovery')
+api = Api(title='Seed Generation', version='1.0', description='Tool to generate seeds for Domain Discovery', doc="/doc")
 
 api.add_namespace(search_api)
 api.add_namespace(classify_api)
