25323b49aef15c22a89fdbe17ed4d49a8aa19053
Tom Barber <tom@spicule.co.uk>
2019-07-24 18:44:57 -0700

    fix endpoint

--- webui/app/apis/ns_search.py
+++ webui/app/apis/ns_search.py
@@ -34,7 +34,7 @@ class SearchPaginated(Resource):
     def get(self, model, query, page):
         """Search Duck Duck Go"""
         try:
-            url_details = search.query_and_fetch(query, model, page=page, top_n=12)
+            url_details = search.query_and_fetch(query, model, page=int(page), top_n=12)
         except DocumentNotFoundError as e:
             print(e)
             raise BadRequest('Model Not Found')
