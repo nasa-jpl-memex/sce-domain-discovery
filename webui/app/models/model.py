a2098170f58292d0ba5c3c8bf5373bcee74a97c9
Tom Barber <tom@spicule.co.uk>
2020-05-01 15:33:39 +0100

    add default urls

--- webui/app/models/model.py
+++ webui/app/models/model.py
@@ -46,7 +46,37 @@ def set_sparkler_defaults(model):
     :param model:
     :return:
     """
-    set_sparkler_options(model, {})
+    topn = 1000
+    topgrp = 256
+    sortby = 'discover_depth asc, score asc'
+    groupby = 'group'
+    serverdelay = 1000
+    fetchheaders = [{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)'
+                                   ' AppleWebKit/537.36 (KHTML, like Gecko) Sparkler'},
+                    {'Accept': 'text/html,application/xhtml+xml,'
+                               'application/xml;q=0.9,image/webp,*/*;q=0.8'},
+                    {'Accept-Language': 'en-US,en'}]
+
+    activeplugins = ['urlfilter-regex', 'urlfilter-samehost', 'scorer-dd-svn']
+    plugins = {'urlfilter.regex': {'urlfilter.regex.file': 'regex-urlfilter.txt'},
+               'fetcher.jbrowser': {'socket.timeout': 3000, 'connect.timeout': 3000},
+               'scorer.dd.svn': {'scorer.dd.svn.url': 'http://sce-api:5000/classify/predict',
+                                 'scorer.dd.svn.fallback': 0, 'scorer.dd.svn.key': 'svn_score'}}
+
+    content = {'crawldb.uri': 'http://sce-solr:8983/solr/crawldb',
+               'spark.master': 'local[*]',
+               'kafka.enable': 'false',
+               'kafka.listeners': 'localhost:9092',
+               'kafka.topic': 'sparkler_%s',
+               'generate.topn': topn,
+               'generate.top.groups': topgrp,
+               'generate.sortby': sortby,
+               'generate.groupby': groupby,
+               'fetcher.server.delay': serverdelay,
+               'fetcher.headers': fetchheaders,
+               'plugins.active': activeplugins,
+               'plugins': plugins}
+    set_sparkler_options(model, content)
 
 
 def set_sparkler_options(model, content):
@@ -56,52 +86,6 @@ def set_sparkler_options(model, content):
     :param content:
     :return:
     """
-    # topn = 1000
-    # topgrp = 256
-    # sortby = 'discover_depth asc, score asc'
-    # groupby = 'group'
-    # serverdelay = 1000
-    # fetchheaders = [{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)'
-    #                                ' AppleWebKit/537.36 (KHTML, like Gecko) Sparkler'},
-    #                 {'Accept': 'text/html,application/xhtml+xml,'
-    #                            'application/xml;q=0.9,image/webp,*/*;q=0.8'},
-    #                 {'Accept-Language': 'en-US,en'}]
-    #
-    # activeplugins = ['urlfilter-regex', 'urlfilter-samehost', 'scorer-dd-svn']
-    # plugins = {'urlfilter.regex': {'urlfilter.regex.file': 'regex-urlfilter.txt'},
-    #            'fetcher.jbrowser': {'socket.timeout': 3000, 'connect.timeout': 3000},
-    #            'scorer.dd.svn': {'scorer.dd.svn.url': 'http://sce-api:5000/classify/predict',
-    #                              'scorer.dd.svn.fallback': 0, 'scorer.dd.svn.key': 'svn_score'}}
-    #
-    # if 'generate.topn' in content:
-    #     topn = content['generate.topn']
-    # if 'generate.top.groups' in content:
-    #     topgrp = content['generate.top.groups']
-    # if 'generate.sortby' in content:
-    #     sortby = content['generate.sortby']
-    # if 'generate.groupby' in content:
-    #     groupby = content['generate.groupby']
-    # if 'fetcher.server.delay' in content:
-    #     serverdelay = content['fetcher.server.delay']
-    # if 'fetcher.headers' in content:
-    #     fetchheaders = content['fetcher.headers']
-    # if 'plugins.active' in content:
-    #     activeplugins = content['plugins.active']
-    # if 'plugins' in content:
-    #     plugins = content['plugins']
-    # content = {'crawldb.uri': 'http://sce-solr:8983/solr/crawldb',
-    #            'spark.master': 'local[*]',
-    #            'kafka.enable': 'false',
-    #            'kafka.listeners': 'localhost:9092',
-    #            'kafka.topic': 'sparkler_%s',
-    #            'generate.topn': topn,
-    #            'generate.top.groups': topgrp,
-    #            'generate.sortby': sortby,
-    #            'generate.groupby': groupby,
-    #            'fetcher.server.delay': serverdelay,
-    #            'fetcher.headers': fetchheaders,
-    #            'plugins.active': activeplugins,
-    #            'plugins': plugins}
     try:
         db_model = get_connection()[model]
         db_model['sparkler_opts'] = content
